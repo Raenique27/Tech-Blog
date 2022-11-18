e2d49a5f955b585de19490e6098b3035a7cfce1b
Raenique27 <raeniquewalters@gmail.com>
2022-11-18 16:55:46 -0500

    added POST and PUT routes to create a post and update a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -70,3 +70,40 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        user_id: req.session.user_id
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({message: 'No post found with this id'});
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
