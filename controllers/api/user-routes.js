98e89e57d474a73632212bb73cda86dd1664abf6
Raenique27 <raeniquewalters@gmail.com>
2022-11-18 13:50:33 -0500

    set up post routes to login and logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { rmdirSync } = require('fs');
 const {User, Post} = require('../../models');
 
 // GET/api/users
@@ -68,4 +69,41 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({message: 'No user with that username!'});
+            return;
+        }
+
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({message: 'Incorrect Password!'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({user: dbUserData, message: 'You are logged in!'});
+        });
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
